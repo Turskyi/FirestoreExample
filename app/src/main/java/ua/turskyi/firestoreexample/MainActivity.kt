d35fcfcc8e36f9a9fee2acb6cbe1b7e3617c46e4
Dmytro Turskyi <dmitriy.turskiy@gmail.com>
2020-01-23 18:27:12 +0200

    realtime updating only a single value

--- app/src/main/java/ua/turskyi/firestoreexample/MainActivity.kt
+++ app/src/main/java/ua/turskyi/firestoreexample/MainActivity.kt
@@ -8,10 +8,7 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
-import com.google.firebase.firestore.DocumentSnapshot
-import com.google.firebase.firestore.EventListener
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.FirebaseFirestoreException
+import com.google.firebase.firestore.*
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
@@ -90,6 +87,19 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
                 Log.d(TAG, e.toString())
             }
     }
+
+    fun updateDescription(v: View?) {
+        val description = editTextDescription!!.text.toString()
+
+        /* creates a new note if it does not exist */
+//        val note: MutableMap<String, Any> = HashMap()
+//        note[KEY_DESCRIPTION] = description
+//        noteRef.set(note, SetOptions.merge())
+
+        /* does not create a new note if it does not exist */
+        noteRef.update(KEY_DESCRIPTION, description)
+    }
+
 
     fun loadNote(v: View?) {
         noteRef.get()
