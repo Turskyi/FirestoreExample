3b7a01e3f1df5d959269c912cdb124870a50529f
Dmytro Turskyi <dmitriy.turskiy@gmail.com>
2020-01-24 15:38:37 +0200

    how to achieve pagination with the Firestore

--- app/src/main/java/ua/turskyi/firestoreexample/MainActivity.kt
+++ app/src/main/java/ua/turskyi/firestoreexample/MainActivity.kt
@@ -6,20 +6,17 @@ import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
-import com.google.android.gms.tasks.Task
-import com.google.android.gms.tasks.Tasks
-import com.google.firebase.firestore.EventListener
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.FirebaseFirestoreException
-import com.google.firebase.firestore.QuerySnapshot
+import com.google.firebase.firestore.Query
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
-    companion object {
+/*    companion object {
         private const val TAG = "MainActivity"
         private const val KEY_TITLE = "title"
         private const val KEY_DESCRIPTION = "description"
-    }
+    }*/
 
     private var editTextTitle: EditText? = null
     private var editTextDescription: EditText? = null
@@ -28,8 +25,9 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     private val db = FirebaseFirestore.getInstance()
     private val notebookRef = db.collection("Notebook")
-    private val noteRef =
-        db.document("Notebook/My First Note")
+/*    private val noteRef = db.document("Notebook/My First Note")*/
+
+    private var lastResult: DocumentSnapshot? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
@@ -41,7 +39,7 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         textViewData = findViewById(R.id.text_view_data)
     }
 
-    override fun onStart() {
+  /*  override fun onStart() {
         super.onStart()
         notebookRef.addSnapshotListener(this, object : EventListener<QuerySnapshot?> {
            override fun onEvent(
@@ -71,7 +69,7 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
                 textViewData!!.text = data
             }
         })
-    }
+    }*/
 
 /*    override fun onStart() {
         super.onStart()
@@ -146,163 +144,18 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         notebookRef.add(note)
     }
 
-/*    fun addNote(v: View?) {
-        val title = editTextTitle!!.text.toString()
-        val description = editTextDescription!!.text.toString()
-        val note =
-            Note(title, description)
-        notebookRef.add(note)
-    }*/
-
-/*    fun saveNote(v: View?) {
-        val title = editTextTitle!!.text.toString()
-        val description = editTextDescription!!.text.toString()
-
-//        val note: MutableMap<String, Any> = HashMap()
-//        note[KEY_TITLE] = title
-//        note[KEY_DESCRIPTION] = description
-
-        *//* nicer way than above *//*
-//        val note = hashMapOf(
-//            KEY_TITLE to title,
-//            KEY_DESCRIPTION to description
-//        )
-
-        *//* working with object *//*
-        val note =
-            Note(title, description)
-
-//        db.collection("Notebook").document("My First Note").set(note)
-        noteRef.set(note)
-            .addOnSuccessListener {
-                Toast.makeText(
-                    this@MainActivity,
-                    "Note saved",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-            .addOnFailureListener { e ->
-                Toast.makeText(this@MainActivity, "Error!", Toast.LENGTH_SHORT).show()
-                Log.d(TAG, e.toString())
-            }
-    }*/
-
-/*    fun updateDescription(v: View?) {
-        val description = editTextDescription!!.text.toString()
-
-        *//* creates a new note if it does not exist *//*
-//        val note: MutableMap<String, Any> = HashMap()
-//        note[KEY_DESCRIPTION] = description
-//        noteRef.set(note, SetOptions.merge())
-
-        *//* does not create a new note if it does not exist *//*
-        noteRef.update(KEY_DESCRIPTION, description)
-    }
-
-    fun deleteDescription(v: View?) {
-//        val note: MutableMap<String, Any> = HashMap()
-//        note[KEY_DESCRIPTION] = FieldValue.delete()
-//        noteRef.update(note)
-
-        *//* the same as above *//*
-        noteRef.update(KEY_DESCRIPTION, FieldValue.delete())
-    }
-
-    fun deleteNote(v: View?) {
-        noteRef.delete()
-    }*/
-
-
-/*    fun loadNote(v: View?) {
-        noteRef.get()
-            .addOnSuccessListener { documentSnapshot ->
-                if (documentSnapshot.exists()) {
-//                    val title = documentSnapshot.getString(KEY_TITLE)
-//                    val description =
-//                        documentSnapshot.getString(KEY_DESCRIPTION)
-
-        //                    val note: MutableMap<String, Any> =
-        //                        documentSnapshot.data as MutableMap<String, Any>
-
-
-                    *//* working with object *//*
-                    val note = documentSnapshot.toObject(Note::class.java)
-                    val title = note?.title
-                    val description = note?.description
-
-                    textViewData?.text = getString(R.string.title_and_description, title, description)
-                } else {
-                    Toast.makeText(
-                        this@MainActivity,
-                        "Document does not exist",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
-            }
-            .addOnFailureListener { e ->
-                Toast.makeText(this@MainActivity, "Error!", Toast.LENGTH_SHORT).show()
-                Log.d(TAG, e.toString())
-            }
-    }*/
-
-/*    fun loadNotes(v: View?) {
-        notebookRef.get()
-            .addOnSuccessListener { queryDocumentSnapshots ->
-                var data = ""
-                for (documentSnapshot in queryDocumentSnapshots) {
-                    val note =
-                        documentSnapshot.toObject(
-                            Note::class.java
-                        )
-                    note.documentId = documentSnapshot.id
-                    val documentId = note.documentId
-                    val title = note.title
-                    val description = note.description
-                    data += ("ID: " + documentId
-                            + "\nTitle: " + title + "\nDescription: " + description + "\n\n")
-                }
-                textViewData!!.text = data
-            }
-    }*/
-
-/*    fun loadNotes(v: View?) {
-        notebookRef.whereGreaterThanOrEqualTo("priority", 2)
-            .orderBy("priority", Query.Direction.DESCENDING)
-            .limit(3)
-            .get()
-            .addOnSuccessListener { queryDocumentSnapshots ->
-                var data = ""
-                for (documentSnapshot in queryDocumentSnapshots) {
-                    val note =
-                        documentSnapshot.toObject(
-                            Note::class.java
-                        )
-                    note.documentId = documentSnapshot.id
-                    val documentId = note.documentId
-                    val title = note.title
-                    val description = note.description
-                    val priority = note.priority
-                    data += ("ID: " + documentId
-                            + "\nTitle: " + title + "\nDescription: " + description
-                            + "\nPriority: " + priority + "\n\n")
-                }
-                textViewData!!.text = data
-            }
-    }*/
-
     fun loadNotes(v: View?) {
-        val task1: Task<*> = notebookRef.whereLessThan("priority", 2)
-            .orderBy("priority")
-            .get()
-        val task2: Task<*> =
-            notebookRef.whereGreaterThan("priority", 2)
-                .orderBy("priority")
-                .get()
-        val allTasks =
-            Tasks.whenAllSuccess<QuerySnapshot>(task1, task2)
-        allTasks.addOnSuccessListener { querySnapshots ->
-            var data = ""
-            for (queryDocumentSnapshots in querySnapshots) {
+        val query: Query = if (lastResult == null) {
+            notebookRef.orderBy("priority")
+                .limit(3)
+        } else {
+            notebookRef.orderBy("priority")
+                .startAfter(lastResult!!)
+                .limit(3)
+        }
+        query.get()
+            .addOnSuccessListener { queryDocumentSnapshots ->
+                var data = ""
                 for (documentSnapshot in queryDocumentSnapshots) {
                     val note =
                         documentSnapshot.toObject(
@@ -317,8 +170,11 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
                             + "\nTitle: " + title + "\nDescription: " + description
                             + "\nPriority: " + priority + "\n\n")
                 }
+                if (queryDocumentSnapshots.size() > 0) {
+                    data += "___________\n\n"
+                    textViewData!!.append(data)
+                    lastResult = queryDocumentSnapshots.documents[queryDocumentSnapshots.size() - 1]
+                }
             }
-            textViewData!!.text = data
-        }
     }
 }
